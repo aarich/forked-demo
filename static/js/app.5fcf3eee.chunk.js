87412c0763ae288816e6621c21bbc49decc798e5
Alex Rich <a.rich@salesforce.com>
2021-11-06 15:57:58 -0700

    Updates

--- static/js/app.5fcf3eee.chunk.js
+++
@@ -1,2 +0,0 @@
-(this.webpackJsonp=this.webpackJsonp||[]).push([[0],{38:function(t,e,n){"use strict";n.d(e,"a",(function(){return i}));var a=n(61),r=n(0),o=n.n(r),c=n(5),s=n(55),u=n(18);function i(){return o.a.createElement(u.a,{style:p.container},o.a.createElement(s.a,null,"Open up App.tsx to start working on your app!"),o.a.createElement(a.a,{style:"auto"}))}var p=c.a.create({container:{flex:1,backgroundColor:"#fff",alignItems:"center",justifyContent:"center"}})},62:function(t,e,n){t.exports=n(84)}},[[62,1,2]]]);
-//# sourceMappingURL=app.5fcf3eee.chunk.js.map
